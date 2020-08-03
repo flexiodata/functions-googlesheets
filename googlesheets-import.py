980dd1fbd2f1afba148b0112ca83a1033c7df3aa
Aaron Williams <aaron@flex.io>
2020-08-03 15:11:58 -0500

    added import scaffolding

---
+++ googlesheets-import.py
@@ -0,0 +1,14 @@
+
+# ---
+# name: googlesheets-import
+# deployed: true
+# title: Google Sheets Import
+# description: Creates functions to access sheets from Google Sheets
+# ---
+
+# main function entry point
+def flex_handler(flex):
+    create_functions(flex)
+
+def create_functions(flex):
+    pass
